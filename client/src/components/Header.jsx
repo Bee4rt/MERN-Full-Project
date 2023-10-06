985ccaea4e81c52010e46d04f2d59eec973452fb
bee4rt <info.visuel@gmail.com>
2023-10-06 19:03:48 +0300

    connect to database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Sahand</span>
-          <span className="text-slate-700">Estate</span>
+          <span className="text-slate-500">bee</span>
+          <span className="text-slate-700">4rt</span>
         </h1>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input

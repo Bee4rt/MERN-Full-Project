c7c7ac5fb4a2b5e4afcef409a5e94072dae48577
bee4rt <info.visuel@gmail.com>
2023-10-06 22:16:35 +0300

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+// import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js';
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
 dotenv.config();
 
 mongoose
@@ -17,3 +22,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000!");
 });
+
+app.use("/api/user", userRouter);
